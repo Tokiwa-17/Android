c4b3acbef0109610cc4432ab7dbacf6cba6a732b
ylf <2872444509@qq.com>
2022-06-18 20:34:35 +0800

    动态详情页面

--- frontend/app/src/main/java/com/example/myapplication/activity/PostDetailActivity.java
+++ frontend/app/src/main/java/com/example/myapplication/activity/PostDetailActivity.java
@@ -11,9 +11,13 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.myapplication.MyService;
 import com.example.myapplication.R;
+import com.example.myapplication.utils.MyImageLoader;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PostDetailActivity extends AppCompatActivity {
 
@@ -21,6 +25,17 @@ public class PostDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_detail);
+        CircleImageView image = findViewById(R.id.pd_avatar);
+        TextView pd_nickname = findViewById(R.id.pd_nickname);
+        TextView pd_content = findViewById(R.id.pd_text);
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        String title = intent.getStringExtra("title");
+        String content = intent.getStringExtra("content");
+        String nickname = intent.getStringExtra("nickname");
+        MyImageLoader.loadImage(image, url);
+        pd_nickname.setText(nickname);
+        pd_content.setText(content);
 
     }
 }
